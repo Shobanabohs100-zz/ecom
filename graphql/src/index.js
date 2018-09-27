b08c6e853988369c1535bc8784c125b1a55a20c7
rahulkavi <letter2shobana@gmail.com>
2018-09-27 12:00:20 +0530

    Basic CRUD operation for a list

--- graphql/src/index.js
+++ graphql/src/index.js
@@ -1,18 +1,75 @@
 const { GraphQLServer } = require("graphql-yoga");
 
-// simple schema that defines a Query type
-const typeDefs = `
-    type Query {
-        info: String!
-    }
-    `
+// mock json 
+let links = [{
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL'
+  }];
+var linksCount = links.length;
+
 // implementation of the schema
 const mocks = {
-    Query:() =>({
-        info: () => `Sample content`
+    Query:() =>(
+        {
+            info: () => `Sample content`,
+            feeds: () => links,
+            link: (root, args) => {
+                console.log(args)
+                let link = null;
+                links.forEach(element => {
+                    if( element.id == args.id ){
+                        link = element;
+                    }
+                });
+                return link;
+            } 
+        }
+    ),
+    Link: () => ({
+        id: (root) => root.id,
+        description: (root) => root.description,
+        url: (root) => root.url,
+    }),
+    Mutation: () => ({
+        postFeed: (root, args) => {
+            const link = {
+                id: `link-${linksCount++}`,
+                url: args.url,
+                description: args.description
+            }
+            links.push(link)
+            return link
+        },
+        updateLink: (root, args) => {
+            let link = null
+            for (let linkIndex = 0; linkIndex < links.length; linkIndex++) {
+                if( links[linkIndex].id == args.id ){
+                    links[linkIndex].url = args.url
+                    links[linkIndex].description = args.description
+                    link = links[linkIndex]
+                }
+            }
+            return link
+        },
+        deleteLink: (root, args) => {
+            let link = null
+            links.forEach( (element, index) => {
+                if( element.id == args.id ){
+                    link = element;
+                    links.splice(index, 1);
+                }
+            });
+            return link
+        }
     })
 }
 
-const server = new GraphQLServer({ typeDefs, resolvers: () => true, mocks })
+const server = new GraphQLServer(
+    { 
+        typeDefs: "./src/schema.graphql", 
+        resolvers: () => true, 
+        mocks 
+    })
 
 server.start(() => console.log("server started at http://localhost:4000") )
